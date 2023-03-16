4d9b251ca47ad0627180a17e7c0fe1027b54202d
Marcos <marcos_maidana@live.com>
2023-03-16 08:05:49 -0300

    task-3

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,67 @@
+const cardsContainer = document.getElementById('articleJs')
+const checkboxesContainer = document.getElementById('checkboxContainer')
+const searchInput = document.querySelector('input')
+
+printCard(amazingEventsData.events)
+crearCheckboxes(amazingEventsData.events)
+
+
+// EVENTS 
+searchInput.addEventListener('input', bothFilters)
+
+checkboxesContainer.addEventListener('change', bothFilters)
+
+// FUNCTIONS
 function printCard(array){
+  if (array.length == 0) {
+    cardsContainer.innerHTML = `<h1 class="display-1 fw-bolder fillearMain">NOT FOUND!</h1>`
+    return
+  }
   let cards = ''
-  for (let event of amazingEventsData.events) {
-    cards += `
-    <div class="card rounded-top-5">
-      <img src="${event.image}" class="card-img-top p-2 rounded-top-5 border-bottom" alt="card picture">
-      <div class="card-body">
-        <h5 class="card-title">${event.name}</h5>
-        <p class="card-text">${event.description}</p>
-        <a href="./details.html" class="btn btn-primary">Details</a>
-      </div>
+  array.forEach(event => {
+    cards += `<div class="card rounded-top-5">
+    <img src="${event.image}" class="card-img-top p-2 rounded-top-5 border-bottom" alt="card picture">
+    <div class="card-body">
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <a href="./details.html" class="btn btn-primary">Details</a>
     </div>
-      `
-    }
-    cardsContainer.innerHTML = cards;
+  </div>`
+  })
+  cardsContainer.innerHTML = cards
 }
 
-const cardsContainer = document.getElementById('articleJs')
-printCard(amazingEventsData)
+function crearCheckboxes(array) {
+  let arrayCat = array.map(event => event.category)
+  let setCat = new Set(arrayCat)
+  let checkboxes = ''
+  setCat.forEach(category => {
+    checkboxes += `<div class="form-check form-check-inline">
+                    <label class="form-check-label"><input class="form-check-input" type="checkbox" id="${category}" value="${category}">${category}</label>
+                  </div>`
+  })
+  checkboxesContainer.innerHTML = checkboxes
+}
+
+function textFilter(array, texto) {
+  let arrayFiltrado = array.filter(element => element.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayFiltrado
+}
+
+function categoryFilter(array){
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  let arrayChecks = Array.from(checkboxes)
+  let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+  let arrayCheckCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+  let arrayFiltrado = array.filter(element => arrayCheckCheckedValues.includes(element.category))
+  if (arrayChecksChecked.length > 0){
+    return arrayFiltrado
+  }
+  return array
+}
+
+function bothFilters() {
+  let firstFilter = textFilter(amazingEventsData.events, searchInput.value)
+  let secondFilter = categoryFilter(firstFilter)
+  printCard(secondFilter)
+}
--- scripts/upcomingEvents.js
+++ scripts/upcomingEvents.js
@@ -1,6 +1,25 @@
-function printUpCard() {
+const cardsContainer = document.getElementById('articleJs')
+const checkboxesContainer = document.getElementById('checkboxContainer')
+const searchInput = document.querySelector('input')
+
+printUpCard(amazingEventsData.events)
+crearCheckboxes(amazingEventsData.events) 
+
+// EVENTS 
+
+searchInput.addEventListener('input', bothFilters)
+
+checkboxesContainer.addEventListener('change', bothFilters)
+
+// FUNCTIONS
+
+function printUpCard(array) {
+  if (array.length == 0) {
+    cardsContainer.innerHTML = `<h1 class="display-1 fw-bolder fillearMain">NOT FOUND!</h1>`
+    return
+  }
   let cards = ''
-  for (let event of amazingEventsData.events) {
+  for (let event of array) {
     if (parseInt(event.date) >= (parseInt(amazingEventsData.currentDate))) {
       cards += `
       <div class="card rounded-top-5">
@@ -14,8 +33,41 @@ function printUpCard() {
       `
     }
   }
-  upContainer.innerHTML = cards
+  cardsContainer.innerHTML = cards
 }
 
-const upContainer = document.getElementById('upjs')
-printUpCard()
+function crearCheckboxes(array) {
+  let arrayCat = array.map(event => event.category)
+  let setCat = new Set(arrayCat)
+  let checkboxes = ''
+  setCat.forEach(category => {
+    checkboxes += `<div class="form-check form-check-inline">
+                    <label class="form-check-label"><input class="form-check-input" type="checkbox" id="${category}" value="${category}">${category}</label>
+                  </div>`
+  })
+  checkboxesContainer.innerHTML = checkboxes
+}
+
+function textFilter(array, texto) {
+  let arrayFiltrado = array.filter(element => element.name.toLowerCase().includes(texto.toLowerCase()))
+  return arrayFiltrado
+}
+
+function categoryFilter(array){
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]')
+  let arrayChecks = Array.from(checkboxes)
+  let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+  let arrayCheckCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+  console.log(arrayCheckCheckedValues)
+  let arrayFiltrado = array.filter(element => arrayCheckCheckedValues.includes(element.category))
+  if (arrayChecksChecked.length > 0){
+    return arrayFiltrado
+  }
+  return array
+}
+
+function bothFilters() {
+  let firstFilter = textFilter(amazingEventsData.events, searchInput.value)
+  let secondFilter = categoryFilter(firstFilter)
+  printUpCard(secondFilter)
+}
